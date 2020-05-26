8073ae924039c4465796ce79befc196983c84a16
Eddy Ogola <edogola01@gmail.com>
2020-05-26 17:03:57 +0300

    html emails, error pages, whitenoise

--- daily_parrot/urls.py
+++ daily_parrot/urls.py
@@ -10,3 +10,8 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('main.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'main.views.page_not_found'
+handler500 = 'main.views.internal_server_error'
+handler403 = 'main.views.permission_denied'
+handler400 = 'main.views.bad_request'
